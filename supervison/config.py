333fedb5c7923cd35109ebe077595cdeb9a9d409
Fikhtengol <lijunchina@foxmail.com>
2013-03-23 15:28:49 +0800

    add requirement

--- supervison/config.py
+++ supervison/config.py
@@ -1,12 +1,8 @@
 import os
-
-workers=['127.0.0.1',
-         '192.168.53.156',
+workers=['192.168.53.156',
          '192.168.53.157',
          '192.168.53.158',
         ]
-supervison='localhost'
 redis_host='localhost'
 redis_port=6379
-input_home=os.path.expanduser('~/workspaces/vulcan/supervison/loginput')
-print input_home
+input_home=os.path.expanduser('~/workspaces/data/')
